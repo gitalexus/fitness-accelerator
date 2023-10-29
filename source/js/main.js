ff8bf5e1932ea5d260e40d5f8c5e95777767037b
gitalexus <ru9.alex@gmail.com>
2023-10-29 14:44:00 +0300

    add: gym block

--- source/js/main.js
+++ source/js/main.js
@@ -2,9 +2,26 @@ import {iosVhFix} from './utils/ios-vh-fix';
 import {initModals} from './modules/modals/init-modals';
 import {Form} from './modules/form-validate/form';
 
-// ---------------------------------
+// идентификатор видео на youtube
+const videoId = '9TZXsZItgdw';
 
+// ---------------------------------
 window.addEventListener('DOMContentLoaded', () => {
+
+  const videoContainer = document.querySelector('.gym__video');
+  const playButton = document.querySelector('.gym__video button');
+  const placeHolder = document.querySelector('.gym__video img');
+
+  if (playButton !== null || videoContainer !== null || placeHolder !== null) {
+    playButton.addEventListener('click', ()=> {
+      const iframe = createIframe(videoId);
+      videoContainer.appendChild(iframe);
+      playButton.remove();
+      placeHolder.remove();
+    });
+  }
+
+  // playButton.addEventListener('click', play);
 
   // Utils
   // ---------------------------------
@@ -48,3 +65,16 @@ window.addEventListener('DOMContentLoaded', () => {
 // breakpointChecker();
 
 // используйте .closest(el)
+
+// Cоздание фрейма с видео
+function createIframe(id) {
+  let iframe = document.createElement('iframe');
+
+  iframe.setAttribute('allow', 'autoplay; encrypted-media; allowfullscreen');
+  iframe.setAttribute('width', '100%');
+  iframe.setAttribute('height', '100%');
+  iframe.setAttribute('frameborder', '0');
+  iframe.setAttribute('src', `https://www.youtube.com/embed/${id}/?rel=0&showinfo=0&autoplay=1&amp;controls=0`);
+
+  return iframe;
+}
